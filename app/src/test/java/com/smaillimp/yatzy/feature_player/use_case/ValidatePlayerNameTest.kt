3eb7306fa9609f1d6da07fbdb9936f136cc0aa11
Andy Moesch <andy.moesch@hotmail.com>
2022-11-17 07:48:37 +0100

    fixup! wip

--- app/src/test/java/com/smaillimp/yatzy/feature_player/use_case/ValidatePlayerNameTest.kt
+++ app/src/test/java/com/smaillimp/yatzy/feature_player/use_case/ValidatePlayerNameTest.kt
@@ -1,16 +1,33 @@
 package com.smaillimp.yatzy.feature_player.use_case
 
-import junit.framework.Assert.assertEquals
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertNull
+import org.junit.Assert.assertTrue
 import org.junit.Test
 
 class TestPlayerNameValidation {
     @Test
     fun `validation fails for empty player name`() {
         val validationResult = ValidatePlayerName().execute("")
-        assert(
-            !validationResult.successful,
-            { "Validation on empty player name should not be successful." }
+        assertFalse(
+            "Validation on empty player name should not be successful.", validationResult.successful
         )
         assertEquals("Please provide a user name.", validationResult.errorMessage)
+    }
+
+    @Test
+    fun `validation succeeds for valid player name`() {
+        val validationResult = ValidatePlayerName().execute("Andy")
+        assertTrue(
+            "Validation for valid name has to be successful.", validationResult.successful
+        )
+        assertNull(
+            (
+                "`validation.errorMessage` should be null " +
+                        "(actual: ${validationResult.errorMessage} with valid player name."
+                ),
+            validationResult.errorMessage
+        )
     }
 }

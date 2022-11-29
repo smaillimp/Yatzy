86c76c8013966770a4b2e201c810ec513bbed63a
Andy Moesch <andy.moesch@hotmail.com>
2022-11-29 17:47:46 +0100

    Update player form view model test to use Mockito

--- app/src/test/java/com/smaillimp/yatzy/presentation/players/form/PlayerFormViewModelTest.kt
+++ app/src/test/java/com/smaillimp/yatzy/presentation/players/form/PlayerFormViewModelTest.kt
@@ -1,15 +1,19 @@
 package com.smaillimp.yatzy.presentation.players.form
 
+import com.smaillimp.yatzy.feature.players.domain.usecase.AddPlayerInterface
+import com.smaillimp.yatzy.feature.players.domain.usecase.GetPlayersInterface
 import com.smaillimp.yatzy.feature.players.model.Player
-import com.smaillimp.yatzy.feature.players.usecase.AddPlayerMock
-import com.smaillimp.yatzy.feature.players.usecase.GetPlayersMock
 import com.smaillimp.yatzy.feature.players.usecase.PlayerUseCases
 import com.smaillimp.yatzy.feature.players.usecase.ValidatePlayerName
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.UnconfinedTestDispatcher
 import kotlinx.coroutines.test.runTest
-import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
+import org.mockito.kotlin.any
+import org.mockito.kotlin.doReturn
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.times
+import org.mockito.kotlin.verify
 
 @ExperimentalCoroutinesApi
 internal class PlayerFormViewModelTest {
@@ -17,16 +21,18 @@ internal class PlayerFormViewModelTest {
     @Test
     fun `event AddPlayer triggers AddPlayer use case`() = runTest {
         val testDispatcher = UnconfinedTestDispatcher(testScheduler)
-        val addPlayerMock = AddPlayerMock()
+        val addPlayerMock = mock<AddPlayerInterface> {
+            onBlocking { invoke(any<Player>()) } doReturn Unit
+        }
         val playerFormViewModel = PlayerFormViewModel(
             PlayerUseCases(
-                getPlayers = GetPlayersMock(),
+                getPlayers = mock<GetPlayersInterface> {},
                 addPlayer = addPlayerMock,
                 validatePlayerName = ValidatePlayerName()
             ),
             defaultDispatcher = testDispatcher
         )
         playerFormViewModel.onEvent(PlayerFormEvent.Submit(Player(name = "Andy")))
-        assertTrue(addPlayerMock.called, "use case AddPlayer must be called after event AddPlayer")
+        verify(addPlayerMock, times(1)).invoke(any<Player>())
     }
 }

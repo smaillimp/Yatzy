ca59a6c0c5ec9b8fa7e2d890999a65b416ffc1ba
Andy Moesch <andy.moesch@hotmail.com>
2023-02-21 17:28:05 +0100

    WIP: shared ViewModel

--- app/src/main/java/com/smaillimp/yatzy/presentation/game/GameScreen.kt
+++ app/src/main/java/com/smaillimp/yatzy/presentation/game/GameScreen.kt
@@ -3,7 +3,6 @@ package com.smaillimp.yatzy.presentation.game
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
--- app/src/main/java/com/smaillimp/yatzy/presentation/game/dice/DiceView.kt
+++ app/src/main/java/com/smaillimp/yatzy/presentation/game/dice/DiceView.kt
@@ -1,15 +1,20 @@
 package com.smaillimp.yatzy.presentation.game.dice
 
+import android.util.Log
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.viewmodel.compose.LocalViewModelStoreOwner
 
 @Composable
-fun DiceView() {
-    val viewModel = hiltViewModel<DiceViewModel>()
+fun DiceView(
+    viewModel: DiceViewModel = hiltViewModel()
+) {
+//    val viewModel = hiltViewModel<DiceViewModel>()
     val state = viewModel.state.value
-
+    Log.i("XX", viewModel.toString())
+    Log.i("XX", LocalViewModelStoreOwner.current.toString())
     Dice(viewModel = viewModel, state = state)
 }
 
